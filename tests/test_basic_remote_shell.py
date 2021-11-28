264397131db01b3f640fc8c0b5bfc37ab1bd16f1
Barnaby Shearer <b@zi.is>
2021-11-28 21:00:35 +0000

    tests

--- tests/test_basic_remote_shell.py
+++ tests/test_basic_remote_shell.py
@@ -5,20 +5,28 @@ import basic_remote_shell
 
 def test_compute_key() -> None:
     """Test we can compute key."""
-    assert basic_remote_shell.compute_key(b"", 0, 0, b"", b"") == b""
+    assert (
+        basic_remote_shell.compute_key(b"", 64, 0, b"", b"")[:8]
+        == b"\x06\r\xc6>U\x95\xdf\xfb"
+    )
 
 
 def test_message() -> None:
     """Text message handling."""
-    msg = basic_remote_shell.Message()
-    assert msg.get_remainder() == b""
+    msg = basic_remote_shell.Message(("\01" * 16).encode())
     assert msg.get_so_far() == b""
     assert msg.get_bytes(0) == b""
-    assert not msg.get_boolean()
-    assert msg.get_int() == 0
+    assert msg.get_boolean()
+    assert msg.get_int() == 16843009
+    assert msg.get_mpint() == 282578800148737
+    msg = basic_remote_shell.Message(("\01" * 8).encode())
+    assert msg.get_binary() == b"\01\01\01\01"
+    msg = basic_remote_shell.Message(("\01" * 8).encode())
+    assert msg.get_boolean()
+    assert msg.get_remainder() == b"\01\01\01\01\01\01\01"
+    assert msg.get_bytes(10) == b"\x01\x01\x01\x01\x01\x01\x01\x00\x00\x00"
+    msg = basic_remote_shell.Message(("\04").encode())
     assert msg.get_mpint() == 0
-    assert msg.get_binary() == b""
-
     msg.add_int(0)
     msg.add_mpint(0)
     msg.add_binary(b"")
